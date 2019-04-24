26e15941765c8c7a835c907ce03d25b67a172922
Lenny Burdette <lenny@squareup.com>
2019-04-24 11:39:16 -0700

    initial commit

---
+++ script/apply-hacks.sh
@@ -0,0 +1,3 @@
+patch node_modules/ember-css-modules/lib/modules-preprocessor.js < $(dirname $0)/modules-preprocessor.js.diff
+
+patch node_modules/ember-css-modules/lib/htmlbars-plugin/index.js < $(dirname $0)/htmlbars-plugin.js.diff
